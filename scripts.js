0695cded476afc1bbe40cfd9806127d1ce146017
ranju stha <ranjustha8844@gmail.com>
2024-10-14 14:37:35 +0545

    wrote objects in array

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const quizData  = [
+    {
+        question: 'how old is Florida?',
+        a: '10',
+        b: '18',
+        c: '26',
+        d: '22',
+        correct: 'c',
+    }, {
+        question: 'What is the most used programming language?',
+        a: 'Java',
+        b: 'C',
+        c: 'JavaScrip',
+        d: 'Python',
+        correct: 'c'
+    }, {
+        question: 'Who is the president of USA?',
+        a: 'Florin Pop',
+        b: 'Donald Trump',
+        c: 'Kamala HArris',
+        d: 'Ivan Sandona',
+        correct: 'b',
+    },  {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Mark Up Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Application Programming Interface',
+        correct: 'a',
+    }, {
+        question: 'What year was JavaScript launched?',
+        a: '2018',
+        b: '2019',
+        c: '2020',
+        d: '1995',
+        correct: 'd',
+    }
+];
+   
